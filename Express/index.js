3f4bdc9f71f51b9cfc263d8aa2cce54c856ca161
MapoDesign <elisaresmini@gmail.com>
2023-09-12 15:26:32 +0200

    Express WebServer test

---
+++ Express/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html', {root: __dirname + '/public'})
+
+})
+
+app.get('/about',(req,res)=>{
+    res.sendFile('about.html', {root: __dirname + '/public'})
+})
+
+app.get('/contact',(req,res)=>{
+    res.sendFile('contact.html', {root: __dirname + '/public'})
+})
+
+app.get('/service',(req,res)=>{
+    res.sendFile('service.html', {root: __dirname + '/public'})
+})
+
+app.all('*',(req,res)=>{
+    res.sendFile('404.html', {root: __dirname + '/public'})
+})
+
+app.listen(3000)
